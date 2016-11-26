86a60db57982b671478b08f2f256fc53d607b893
Jony <jonysy@users.noreply.github.com>
2016-11-25 21:46:03 -0500

    Add Size struct

--- src/plane/mod.rs
+++ src/plane/mod.rs
@@ -1,2 +1,5 @@
 pub use self::point::Point;
+pub use self::size::Size;
+
 mod point;
+mod size;
---
+++ src/plane/size.rs
@@ -0,0 +1,49 @@
+use std::fmt;
+use std::ops::Mul;
+
+pub struct Size<I = f64> {
+    /// The width.
+    pub width: I,
+    /// The height.
+    pub height: I,
+}
+
+impl<I> Size<I> {
+    pub fn area(&self) -> I::Output where I: Copy + Mul {
+        self.width * self.height
+    }
+}
+
+impl<I> Clone for Size<I> where I: Clone {
+    fn clone(&self) -> Self {
+        Size { width: self.width.clone(), height: self.height.clone() }
+    }
+}
+
+impl<I> Copy for Size<I> where I: Copy { }
+
+impl<I> Eq for Size<I> where I: Eq { }
+
+impl<I> PartialEq for Size<I> where I: PartialEq {
+    fn eq(&self, other: &Self) -> bool {
+        self.width == other.width && self.height == other.height
+    }
+}
+
+impl<I> fmt::Display for Size<I> where I: fmt::Display {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}x{}", self.width, self.height)
+    }
+}
+
+impl<I> From<(I, I)> for Size<I> {
+    fn from((width, height): (I, I)) -> Self {
+        Size { width, height }
+    }
+}
+
+impl<I> From<[I; 2]> for Size<I> {
+    fn from([width, height]: [I; 2]) -> Self {
+        Size { width, height }
+    }
+}
