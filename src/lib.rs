02706e7c78c45df26bf9e76bf2c8f2b629994f23
Jony <jonysy@users.noreply.github.com>
2016-11-29 16:10:11 -0500

    Add crates

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,6 @@
-pub extern crate core;
-pub extern crate euclidean;
+pub extern crate caribou_core as core;
+pub extern crate caribou_euclidean as euclidean;
+pub extern crate caribou_ext as ext;
+pub extern crate caribou_image as image;
+pub extern crate caribou_linear as linear;
+pub extern crate caribou_motion as motion;
