a14769ae8ee4f8ce48a12391ed782d998dab8b74
Jony <jonysy@users.noreply.github.com>
2016-12-04 04:57:59 -0500

    Update package name

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
-pub extern crate caribou_core as core;
-pub extern crate caribou_euclidean as euclidean;
-pub extern crate caribou_ext as ext;
-pub extern crate caribou_image as image;
-pub extern crate caribou_linear as linear;
-pub extern crate caribou_motion as motion;
-pub extern crate caribou_tracking as tracking;
+pub extern crate lychee_core as core;
+pub extern crate lychee_euclidean as euclidean;
+pub extern crate lychee_ext as ext;
+pub extern crate lychee_image as image;
+pub extern crate lychee_linear as linear;
+pub extern crate lychee_motion as motion;
+pub extern crate lychee_tracking as tracking;
