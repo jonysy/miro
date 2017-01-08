d0e0578d1fa36af05f9681d582149c2d79963667
Jony <jonysy@users.noreply.github.com>
2017-01-08 17:34:42 -0500

    Prep for merge

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 #![feature(field_init_shorthand, inclusive_range_syntax, non_ascii_idents)]
 
-extern crate caribou_core as core;
-extern crate caribou_euclidean as euclidean;
-extern crate caribou_ext as ext;
-extern crate caribou_image as image;
+extern crate lychee_core as core;
+extern crate lychee_euclidean as euclidean;
+extern crate lychee_ext as ext;
+extern crate lychee_image as image;
 extern crate extensions;
 
 pub use self::median_flow::MedianFlow;
