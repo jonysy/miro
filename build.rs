564efe298a9cdc0537d94fc42c8e88a7c3b928ad
Jony <jonysy@users.noreply.github.com>
2017-01-14 02:12:02 -0500

    Prep for v2

---
+++ build.rs
@@ -0,0 +1,31 @@
+use std::{env, fs, io};
+use std::io::Write;
+
+fn main() {
+
+	if let Err(error) = copy("main.rs") {
+
+		panic!("{}", error);
+	}
+}
+
+fn copy(name: &str) -> io::Result<()> {
+
+	let mut inp = fs::File::open(name)?;
+
+	let mut f = {
+
+		let dir = env::var("OUT_DIR").unwrap();
+		let path = dir + "/main.rs";
+		fs::File::create(&path)?
+	};
+
+	writeln!(f, "{{")?;
+
+	let _ = io::copy(&mut inp, &mut f)?;
+
+	writeln!(f, "Ok(())")?;
+	writeln!(f, "}}")?;
+
+	Ok(())
+}
