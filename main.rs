3aee956235a0f15e97f0435a6957d3abb28cd928
Jony <jonysy@users.noreply.github.com>
2017-01-20 22:52:57 -0500

    Add drag fn

--- main.rs
+++ main.rs
@@ -1,12 +1,8 @@
 use high::{capture, piston};
-use miro::misc::parcmp;
 
-// use miro::extn::Flow;
-// use miro::motion::PyrLk;
-
+const COLOR: [f32; 4] = [1.0, 0.0, 0.0, 0.20];
 let mut pressed = false;
-let mut rectangle = [0.0, 0.0, 50.0, 50.0];
-let (width, height) = piston::window_size();
+let mut rectangle = None::<[f64; 4]>;
 
 capture::conn();
 
@@ -17,7 +13,11 @@ capture::conn();
 		piston::clear([1.0; 4])?;
 		let image = capture::read();
 		piston::draw_image(&image)?;
-		piston::draw_rectangle([1.0, 0.0, 0.0, 1.0], rectangle)?;
+
+		if let Some(r) = rectangle {
+
+			piston::draw_rectangle(COLOR, r)?
+		}
 	}
 
 	// =========
@@ -31,18 +31,31 @@ capture::conn();
 	if Some(1) == piston::pressed_mouse_button() || Some(1) == piston::released_mouse_button(){
 
 		info!("Left mouse button pressed/released");
+		if !pressed { rectangle = None; }
 		pressed = !pressed;
 	}
 
-	if pressed {
-
-		if let Some(position) = piston::mouse_cursor_position() {
-			rectangle[0] = position[0];
-			rectangle[1] = position[1];
-
-			//println!("{:?}", position);
-		}
-	}
+	update_drag_state(pressed, &mut rectangle);
 }
 
 capture::disconn();
+
+fn update_drag(pressed: bool, rectangle: &mut Option<[f64; 4]>) {
+	if pressed {
+
+		if let Some(position) = piston::mouse_cursor_position() {
+
+			match rectangle {
+				&mut Some([x, y, ref mut dim..]) => {
+
+					dim[0] = position[0] - x;
+					dim[1] = position[1] - y;
+				},
+
+				r @ _ => {
+					*r = Some([position[0], position[1], 0.0, 0.0]);
+				}
+			}
+		}
+	}
+}
